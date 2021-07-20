a14fce84b750973d5db3e55a9f05f382f464ee3c
guohao <guohao@DESKTOP-I72ILIH.localdomain>
2021-07-21 00:10:05 +0800

    deciding on what to do for states

--- src/components/calculators/RetirePlan.js
+++ src/components/calculators/RetirePlan.js
@@ -10,7 +10,6 @@ function RetirePlan(props) {
     // const [accPeriod, setAccPeriod] = useState(0)
     function handleChange(e) {
         setRetireForm({...retireForm, [e.target.name] : e.target.value})
-        // getResults()
     }
 
     console.log(retireForm.retireAge)
@@ -26,6 +25,7 @@ function RetirePlan(props) {
     const [annualInflation, setAnnualInflation] = useState(0)
     const [realReturns, setRealReturns] = useState(0)
     const [disabled, setDisabled] = useState(false)
+
     let data = singstat.default.Level1
     let infData = []
     for(let i = 0; i < data.length; i++){
@@ -36,7 +36,7 @@ function RetirePlan(props) {
             }
         }
     }
-    console.log(disabled)
+    console.log(infData)
     let accPeriod = 0;
     if(accPeriod > data[data.length-1].year - data[0].year){
         accPeriod = (data[data.length-1].year - data[0].year)
@@ -45,27 +45,48 @@ function RetirePlan(props) {
     }else{
         accPeriod = retireForm.retireAge - retireForm.startAge
     }
-
-    console.log(data[data.length-1].year - data[0].year)
-    // let accPeriod = 0
     useEffect(()=>{
-        getResults()
+        enterRetirePlan()
     },[retireForm])
 
-    async function getResults(e) {
+    // const [infRetireFund, setInfRetireFund] = useState(0)
+    // const [fvStartFund, setFvStartFund] = useState(0)
+    //
+    // const[reqInfRetireFund, setReqInfRetireFund] = useState(0) //required fund = PV annuity - FV of starting fund
+    //
+    // const [infRetireSavings, setInfRetireSavings] = useState(0)
+    async function enterRetirePlan(e) {
         setDrawdownPeriod(retireForm.lifeExp - retireForm.retireAge)
-        setEndInfYear(infData[infData.length-1]?.year)
+        setEndInfYear(infData[infData.length-1]?.year) //2020
         setStartInfYear(infData[infData.length - accPeriod]?.year)
-        setEndInfValue(infData[infData.length-1]?.value)
+        setEndInfValue(infData[infData.length-1]?.value) //2020
         setStartInfValue(infData[infData.length - accPeriod]?.value)
-        await setCumInflation((-(startInfValue - endInfValue) / startInfValue)*100)
-        await setAnnualInflation(((endInfValue/startInfValue)**(1/accPeriod)-1)*100 )
-        setRealReturns(retireForm.expReturns - annualInflation)
+        setCumInflation((-(startInfValue - endInfValue) / startInfValue)*100)
+        setAnnualInflation(((endInfValue/startInfValue)**(1/accPeriod)-1)*100 )
+        setRealReturns((retireForm.expReturns - annualInflation)/100) //in percentage
+        }
+    let vRetireFund = (retireForm.retireExpense * drawdownPeriod)
+    let vRetireSaving = ( (retireForm.retireExpense * drawdownPeriod - retireForm.startFund) / accPeriod)
 
-    }
+    let infRetireFund = (retireForm.retireExpense* ((1-(1+realReturns)**(-drawdownPeriod))/realReturns) *(1+realReturns))
+    let fvStartFund = (retireForm.startFund*((1+realReturns)**accPeriod))
+    let reqInfRetireFund = (infRetireFund-fvStartFund)
+    let infRetireSavings = ((reqInfRetireFund/(1+realReturns)) / (( ((1+realReturns)**accPeriod) -1)/realReturns) )
+
+    console.log(realReturns)
+    console.log(infRetireFund) //
+    console.log(fvStartFund) //
+    console.log(reqInfRetireFund)//
+    console.log(infRetireSavings)//
+
+    console.log(vRetireFund)
+    console.log(vRetireSaving)
+
     function disableButton(){
         setDisabled(true)
     }
+
+    console.log(disabled)
     const inputRef1 = useRef()
     const inputRef2 = useRef()
     const inputRef3 = useRef()
@@ -84,20 +105,13 @@ function RetirePlan(props) {
         inputRef6.current.value = 0
         setDisabled(false)
     }
-    console.log(realReturns)
-    let cumInflation1 = (-(startInfValue - endInfValue) / startInfValue)*100
-    console.log(cumInflation1)
-    console.log(`cumInflation ${cumInflation} from ${startInfYear} (${startInfValue}) to ${endInfYear} (${endInfValue})`)
-    let annualInflation1 = (endInfValue/startInfValue)**(1/accPeriod)-1 // CAGR = (FV-PV)^1/n -1
-    console.log(`annualInflation1 ${annualInflation1}`)
-    console.log(`annualInflation ${annualInflation}`)
 
     let abc= 1234
 
     return (
         <Row>
             <h1>{accPeriod}</h1>
-            <h5>cumInf={cumInflation} annInf={annualInflation} realReturns={realReturns}</h5>
+            <h5>from {startInfYear} to {endInfYear} cumInf={cumInflation} annInf={annualInflation} realReturns={realReturns}</h5>
             <h3 className="col-12 mt-3 mb-5 text-center">Retirement Planner</h3>
             <Col className="col-6">
                 <Row >
@@ -114,14 +128,14 @@ function RetirePlan(props) {
                     <Col className="col-5 col-md-5 text-end mt-1">Expected returns (%):</Col>
                         <input ref={inputRef6} className="col-7 col-md-7 mb-3"  name="expReturns" type="number" placeholder="Assumed at 0.05% bank savings interests if left blank" onChange={handleChange} />
                     <Col className="col-5 col-md-5 "></Col>
-                        {!disabled ?
-                            <Button className="col-7 col-md-7" onClick={getResults && disableButton}>Get Results</Button>
-                            :
-                            <Button className="col-3 col-md-3 bg-info " onClick={refreshForm}>Refresh</Button>
-                        }
+                        {/*{!disabled ?*/}
+                            <Button className="col-7 col-md-7" onClick={enterRetirePlan} >Get Results</Button>
+                        //     :
+                        //     <Button className="col-3 col-md-3 bg-info " onClick={refreshForm}>Refresh</Button>
+                        // }
                 </Row>
             </Col>
-            {disabled ?
+            {/*{disabled ?*/}
                 <Col className="col-6 col-md-6">
                     <Row>
                         <Col className="col-12 col-md-12 text-center ">Vanilla Savings
@@ -129,26 +143,26 @@ function RetirePlan(props) {
                                 <Col className="col-6 col-md-6 text-center my-2 ">Retirement Fund Required:</Col>
                                 <Col className="col-6 col-md-6 text-center my-2 ">Annual Savings Required:</Col>
                                 <input className="col-5 col-md-5 text-center mx-3 " name="" type="number"
-                                       placeholder={abc} disabled={true}/>
+                                       placeholder={vRetireFund} disabled={true}/>
                                 <input className="col-5  col-md-5 text-center mx-3" name="" type="number"
-                                       placeholder={abc} disabled={true}/>
+                                       placeholder={vRetireSaving} disabled={true}/>
                             </Row>
                             <Col className="mt-5">Inflation Adjusted Savings</Col>
                             <Row className="d-flex justify-content-center ">
                                 <Col className="col-6 col-md-6 text-center my-1 ">Retirement Fund Required:</Col>
                                 <Col className="col-6 col-md-6 text-center my-1">Annual Savings Required:</Col>
                                 <input className="col-5 col-md-5 text-center mx-3 " name="" type="number"
-                                       placeholder={abc} disabled={true}/>
+                                       placeholder={infRetireFund} disabled={true}/>
                                 <input className="col-5  col-md-5 text-center mx-3" name="" type="number"
-                                       placeholder={abc} disabled={true}/>
+                                       placeholder={infRetireSavings} disabled={true}/>
                             </Row>
                             <Button className="mt-md-5 w-50">Save Retirement Plan</Button>
                         </Col>
                     </Row>
                 </Col>
-                :
-                <></>
-            }
+                {/*:*/}
+            {/*    <></>*/}
+            {/*}*/}
         </Row>
     )
         ;
